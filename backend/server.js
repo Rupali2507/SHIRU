4c85402c732c4abb253ea33d4775882f2fd59cb3
Rupali2507 <rupalik.ug23.ma@nitp.ac.in>
2026-09-05 20:05:33 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,34 @@
-import express from 'express';
-import connectDB from './config/connectDB.js';
-import dotenv from 'dotenv'
-import cors from 'cors'
-import authRoutes from './routes/authRoutes.js'
+import express from "express";
+import connectDB from "./config/connectDB.js";
+import dotenv from "dotenv";
+import cors from "cors";
+
+import authRoutes from "./routes/authRoutes.js";
 import merchantRoutes from "./routes/merchantRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 import aiRoutes from "./routes/aiRoutes.js";
 
 dotenv.config();
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
 connectDB();
 
-app.get('/',(req,res)=>{
-    res.json({
-        success: true,
-        message: "Shiru backend is running"
-    })
-})
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Shiru backend is running",
+  });
+});
 
 app.use("/api/auth", authRoutes);
 app.use("/api/merchant", merchantRoutes);
 app.use("/api/products", productRoutes);
-app.use("/api/orders",orderRoutes);
+app.use("/api/orders", orderRoutes);
 app.use("/api/ai", aiRoutes);
 
-app.listen(process.env.PORT,()=>{
-    console.log(`Server is runnning at ${process.env.PORT}`)
-})
+export default app;
